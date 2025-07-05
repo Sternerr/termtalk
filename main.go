09bf2f22f5fb3aad3ce5d362cd9dbf9ead4861df
Noah <volpy2000@gmail.com>
2025-07-05 12:23:33 +0200

    accept one connection

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
 	fmt.Println("Chat Server")
+
+	addr := "localhost:42069"
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Client Connected, %s", conn)
+	for {}
 }
